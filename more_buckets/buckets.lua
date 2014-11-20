f8c4bc4979e818c34967a91133b07e7e31cd9ce4
unknown <armaorob@yahoo.fr>
2014-11-20 19:52:10 +0100

    Ajout du silver oublie + Supression de l'ancien init

--- more_buckets/buckets.lua
+++ more_buckets/buckets.lua
@@ -1,5 +1,5 @@
 -- Registration of buckets for more_buckets
--- Last modification on Thursday, November 20th by Mg
+-- Last modification on Thursday, November 20th by Ataron
 
 more_buckets.register_bucket("wood",{
 	recipeitem = "default:wood",
@@ -21,7 +21,7 @@ more_buckets.register_bucket("bronze",{
 	recipeitem = "moreores:bronze_ingot",
 	description = "Bronze Bucket",
 	inventory_image = "bronze_bucket.png",
-	liquids = {"default:water_source"},
+	liquids = {"default:water_source", "default:lava_source"},
 	sounds = default.node_sound_wood_defaults()
 })
 	
@@ -35,7 +35,7 @@ more_buckets.register_bucket("copper", {
 		
 more_buckets.register_bucket("gold",{
 		recipeitem = "default:gold_ingot",
-		description = "Gold test bucket",
+		description = "Gold Bucket",
 		inventory_image = "gold_bucket.png",
 		liquids = {"default:lava_source"},
 		sounds = default.node_sound_wood_defaults()
@@ -46,6 +46,13 @@ if minetest.get_modpath("moreores") ~= nil then
 		recipeitem = "moreores:tin_ingot",
 		description = "Tin Bucket",
 		inventory_image = "tin_bucket.png",
+		liquids = {"default:water_source"},
+		sounds = default.node_sound_wood_defaults()
+	})
+		more_buckets.register_bucket("silver",{
+		recipeitem = "moreores:silver_ingot",
+		description = "Silver Bucket",
+		inventory_image = "silver_bucket.png",
 		liquids = {"default:water_source"},
 		sounds = default.node_sound_wood_defaults()
 	})
