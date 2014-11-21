900fb388bd9e503e646a4fe951ab1d0b1812c63d
LeMagnesium <mg.minetest@gmail.com>
2014-11-21 21:08:20 +0100

    Adding a few liquids - Adding liquids from turbogus' fluide mod

--- more_buckets/liquids.lua
+++ more_buckets/liquids.lua
@@ -4,6 +4,8 @@
 -- Default ones
 more_buckets.register_liquid({source_name = "default:water_source",filling_texture = "eau_d.png"})
 more_buckets.register_liquid({source_name = "default:lava_source",filling_texture = "lava_d.png"})
+
+-- If acid_source exists (it actually is in Carbone), add it
 if minetest.registered_nodes["default:acid_source"] then
 	more_buckets.register_liquid({source_name = "default:acid_source",filling_texture = "eau_d.png"})
 end
@@ -14,4 +16,11 @@ if minetest.get_modpath("lulzpack") ~= nil then
 	more_buckets.register_liquid({source_name = "lulzpack:meltedbretonium_source", filling_texture = "lava_d.png"})
 	more_buckets.register_liquid({source_name = "lulzpack:meltediron_source", filling_texture = "lava_d.png"})
 	more_buckets.register_liquid({source_name = "lulzpack:meltedununterx_source", filling_texture = "lava_d.png"})
+end
+
+-- Liquids from fluide mod
+if minetest.get_modpath("fluide") ~= nil then
+	more_buckets.register_liquid({source_name = "fluide:seve_source", filling_texture = "lava_d.png"})
+	more_buckets.register_liquid({source_name = "fluide:or_source", filling_texture = "lava_d.png"})
+	more_buckets.register_liquid({source_name = "fluide:gazole_source", filling_texture = "lava_d.png"})
 end
